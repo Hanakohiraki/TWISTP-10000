e1b5ea1b82e91403c8b3d273d3f385f23738c0c4
HanaK <123821106+Hanakohiraki@users.noreply.github.com>
2025-07-19 18:06:58 +0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Boot from './ui/boot';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Boot />
+  </React.StrictMode>
+);
