a1bc5328ece9a5d2f9e108ac08e6e900ec165bb0
HanaK <123821106+Hanakohiraki@users.noreply.github.com>
2025-07-19 19:34:39 +0700

    Update and rename main.tsx to main.ts

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '../dist/index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
